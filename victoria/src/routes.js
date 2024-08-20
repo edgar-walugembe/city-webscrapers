b7ca8d3b0e5de1f8112b35aaa865cda3db634a49
PC <edgar.walugembe.upti@gmail.com>
2024-08-20 12:56:10 +0300

    all cities 0001

--- victoria/src/routes.js
+++ victoria/src/routes.js
@@ -19,140 +19,140 @@ router.addHandler("DETAIL", async ({ request, page, log, dataset }) => {
   const make = carMatch ? carMatch[2] : "Make Not Found";
   const model = carMatch ? carMatch[3] : "Model Not Found";
 
+  //Car Price
+  const carPrice =
+    (await page.locator("span#final-price").textContent()) || "Not Available";
+
   console.log(`Year: ${year}`);
   console.log(`Make: ${make}`);
   console.log(`Model: ${model}`);
+  console.log(`Price: ${carPrice}`);
 
   //Car Name
-  const carNameWithTrim =
-    (await page.locator("h1.hero-title").textContent()) || "Not Available";
+  // const carNameWithTrim =
+  //   (await page.locator("h1.hero-title").textContent()) || "Not Available";
 
-  const carTrim =
-    (await page.locator("span#spec-value-2").textContent()) || "Not Available";
+  // const carTrim =
+  //   (await page.locator("span#spec-value-2").textContent()) || "Not Available";
 
-  function getCarName(carNameWithTrim, carTrim) {
-    const trimIndex = carNameWithTrim.indexOf(carTrim);
+  // function getCarName(carNameWithTrim, carTrim) {
+  //   const trimIndex = carNameWithTrim.indexOf(carTrim);
 
-    if (trimIndex !== -1) {
-      return carNameWithTrim.substring(0, trimIndex).trim();
-    } else {
-      return carNameWithTrim;
-    }
-  }
-
-  const carName = getCarName(carNameWithTrim, carTrim);
-
-  //Car Year
-  const pattern = /\b\d{4}\b/;
-  const match = carName.match(pattern);
-  const carYear = match ? match[0] : "No Year Found";
-
-  //Car Image
-  const carImage =
-    (await page.locator("img#mainPhoto.loaded").getAttribute("src")) ||
-    "Not Available";
-
-  //Other Images
-  await page.click("img#mainPhoto.loaded");
-
-  await page.waitForSelector(".gallery-thumbnail img");
-
-  const otherCarImages = await page.$$eval(".gallery-thumbnail img", (imgs) =>
-    imgs.map((img) => img.src)
-  );
-
-  //Car Condition
-  const carStatus =
-    (await page.locator("span#spec-value-1").textContent()) || "Not Available";
-
-  //Car Body Type
-  const carBodyType =
-    (await page.locator("span#spec-value-3").textContent()) || "Not Available";
-
-  //Car Engine
-  const carEngine =
-    (await page.locator("span#spec-value-4").textContent()) || "Not Available";
-
-  //Car DriveTrain
-  const carDrivetrain =
-    (await page.locator("span#spec-value-7").textContent()) || "Not Available";
-
-  //Car Price
-  const carPrice =
-    (await page.locator(".pa-current-asking-price").textContent()) ||
-    "Not Available";
-
-  //Car Mileage
-  const carMileage =
-    (await page.locator(".ca-current-mileage").textContent()) ||
-    "Not Available";
-
-  //Number of Car Doors
-  const carDoors =
-    (await page.locator("span#spec-value-12").textContent()) || "Not Available";
-
-  //Car Color
-  const carExteriorColor =
-    (await page.locator("span#spec-value-9").textContent()) || "Not Available";
-
-  const carInteriorColor =
-    (await page.locator("span#spec-value-10").textContent()) || "Not Available";
-
-  //Car Fuel Type
-  const carFuelType =
-    (await page.locator("span#spec-value-13").textContent()) || "Not Available";
-
-  //Car Transmission
-  const carTransmission =
-    (await page.locator("span#spec-value-6").textContent()) || "Not Available";
-
-  //Car Description
-  const carDescription =
-    (await page.locator("div#vdp-collapsible-short-text").textContent()) ||
-    "Not Available";
-
-  const carDetails = {
-    car_url: request.url,
-    car_id: uuidv4(),
-    carManufacturer,
-    carName,
-    carYear,
-    carImage,
-    otherCarImages,
-    carStatus,
-    carMileage,
-    carPrice,
-    carBodyType,
-    carTrim,
-    carEngine,
-    carDrivetrain,
-    carDoors,
-    carExteriorColor,
-    carInteriorColor,
-    carFuelType,
-    carTransmission,
-    carDescription,
-  };
-
-  log.debug(`Saving data: ${request.url}`);
-
-  await dataset.pushData(carDetails);
-
-  // this will be useful for the scheduled actor..
-
-  // const existingData = await dataset.getData();
-  // const isDuplicate = existingData.items.some(
-  //   (item) => item.url === carDetails.url
-  // );
-
-  // if (!isDuplicate) {
-  //   await dataset.pushData(carDetails);
-  //   console.log(carDetails);
-  // } else {
-  //   log.debug(`Duplicate data found: ${request.url}`);
+  //   if (trimIndex !== -1) {
+  //     return carNameWithTrim.substring(0, trimIndex).trim();
+  //   } else {
+  //     return carNameWithTrim;
+  //   }
   // }
 
-  console.log(carDetails);
+  // const carName = getCarName(carNameWithTrim, carTrim);
+
+  // //Car Year
+  // const pattern = /\b\d{4}\b/;
+  // const match = carName.match(pattern);
+  // const carYear = match ? match[0] : "No Year Found";
+
+  // //Car Image
+  // const carImage =
+  //   (await page.locator("img#mainPhoto.loaded").getAttribute("src")) ||
+  //   "Not Available";
+
+  // //Other Images
+  // await page.click("img#mainPhoto.loaded");
+
+  // await page.waitForSelector(".gallery-thumbnail img");
+
+  // const otherCarImages = await page.$$eval(".gallery-thumbnail img", (imgs) =>
+  //   imgs.map((img) => img.src)
+  // );
+
+  // //Car Condition
+  // const carStatus =
+  //   (await page.locator("span#spec-value-1").textContent()) || "Not Available";
+
+  // //Car Body Type
+  // const carBodyType =
+  //   (await page.locator("span#spec-value-3").textContent()) || "Not Available";
+
+  // //Car Engine
+  // const carEngine =
+  //   (await page.locator("span#spec-value-4").textContent()) || "Not Available";
+
+  // //Car DriveTrain
+  // const carDrivetrain =
+  //   (await page.locator("span#spec-value-7").textContent()) || "Not Available";
+
+  //Car Mileage
+  // const carMileage =
+  //   (await page.locator(".ca-current-mileage").textContent()) ||
+  //   "Not Available";
+
+  // //Number of Car Doors
+  // const carDoors =
+  //   (await page.locator("span#spec-value-12").textContent()) || "Not Available";
+
+  // //Car Color
+  // const carExteriorColor =
+  //   (await page.locator("span#spec-value-9").textContent()) || "Not Available";
+
+  // const carInteriorColor =
+  //   (await page.locator("span#spec-value-10").textContent()) || "Not Available";
+
+  // //Car Fuel Type
+  // const carFuelType =
+  //   (await page.locator("span#spec-value-13").textContent()) || "Not Available";
+
+  // //Car Transmission
+  // const carTransmission =
+  //   (await page.locator("span#spec-value-6").textContent()) || "Not Available";
+
+  // //Car Description
+  // const carDescription =
+  //   (await page.locator("div#vdp-collapsible-short-text").textContent()) ||
+  //   "Not Available";
+
+  // const carDetails = {
+  //   car_url: request.url,
+  //   car_id: uuidv4(),
+  //   carManufacturer,
+  //   carName,
+  //   carYear,
+  //   carImage,
+  //   otherCarImages,
+  //   carStatus,
+  //   carMileage,
+  //   carPrice,
+  //   carBodyType,
+  //   carTrim,
+  //   carEngine,
+  //   carDrivetrain,
+  //   carDoors,
+  //   carExteriorColor,
+  //   carInteriorColor,
+  //   carFuelType,
+  //   carTransmission,
+  //   carDescription,
+  // };
+
+  // log.debug(`Saving data: ${request.url}`);
+
+  // await dataset.pushData(carDetails);
+
+  // // this will be useful for the scheduled actor..
+
+  // // const existingData = await dataset.getData();
+  // // const isDuplicate = existingData.items.some(
+  // //   (item) => item.url === carDetails.url
+  // // );
+
+  // // if (!isDuplicate) {
+  // //   await dataset.pushData(carDetails);
+  // //   console.log(carDetails);
+  // // } else {
+  // //   log.debug(`Duplicate data found: ${request.url}`);
+  // // }
+
+  // console.log(carDetails);
 });
 
 router.addHandler("CATEGORY", async ({ page, enqueueLinks, request, log }) => {
